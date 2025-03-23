58d5265e0f3e5356b24a79a9c88a1ecd166b3309
Fernando Perry <fernandoperry1234@gmail.com>
2025-03-23 12:03:15 +0700

    stuff.

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -9,18 +9,24 @@ Game::Game() {
 Game::~Game() {}
 
 void Game::init(Window *w) {
+    mWindow_ptr = w;
     Texture2D *white = mTexMan.loadAssets("white_beads", "./assets/white.png");
     sptr_t<Object> obj =
         mObjMan.addObject(Object({0, 0, 100, 100}, 1, "player1", white));
 }
 
-void Game::handle_logic(float dt) {}
+void Game::handle_logic(float dt) {
+    (void) dt;
+}
 
 void Game::handle_drawing(float dt) {
+    (void) dt;
     for (auto &[_, object] : mObjMan.mData) {
         object->render();
     }
 }
 
 void Game::handle_key(float dt) {
+    (void) dt;
+    /*if (IsKeyPressed(KEY_F)) SetWindowState(FLAG_FULLSCREEN_MODE);*/
 }
