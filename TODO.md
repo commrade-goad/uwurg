da4e3b85e3155b99dca192544d52b4d3708e169c
Fernando Perry <fernandoperry1234@gmail.com>
2025-05-13 13:14:45 +0700

    fix: working on adding point to the bot.

--- TODO.md
+++ TODO.md
@@ -39,6 +39,7 @@
 - [ ] the game itself
 - [ ] Character
 - [ ] Sprite for the bead
+- [ ] Add point to the bot.
 - [x] Enemy AI (Maybe DONE?)
 - [ ] Add little bit of delay for the enemy think (so the player can see what move they do.)
 - [ ] Ketumpuk ke bawah saat tengah (IDK)
--- src/Game/GameBot.cpp
+++ src/Game/GameBot.cpp
@@ -21,12 +21,13 @@ void GameBot::bot_move() {
         a->mShow = false;
         a->mPos = 10000;
         mGame_ptr->mScore[(int)mGame_ptr->mTurn] += 1;
+        TraceLog(LOG_INFO, TextFormat("Bot do the finish move with the bead name %s to this pos %d", move.mBead->mName.c_str(), move.mNewPos));
         break;
     case MoveType::NEWBEAD:
         a->mPos = move.mNewPos;
         a->mOut = true;
         a->mShow = true;
-        TraceLog(LOG_INFO, "doing new bead move");
+        TraceLog(LOG_INFO, TextFormat("Bot do the new bead move with the bead name %s to this pos %d", move.mBead->mName.c_str(), move.mNewPos));
         break;
     case MoveType::MOVEBEAD:
         a->mPos = move.mNewPos;
@@ -35,6 +36,12 @@ void GameBot::bot_move() {
             en_obj->mPos = 0;
             en_obj->mOut = false;
         }
+        TraceLog(LOG_INFO, TextFormat("Bot do the bead move with the bead name %s to this pos %d", move.mBead->mName.c_str(), move.mNewPos));
+
+        // TODO: add point to the bot.
+        if (a->mPos == 15) {
+            mGame_ptr->mScore[(int)GameTurn::PLAYER2] += 1;
+        }
         break;
     default:
         break;
@@ -42,8 +49,12 @@ void GameBot::bot_move() {
     _ingame_getdice(mGame_ptr);
     if (!move.mExtraTurn)
         game_change_turn(mGame_ptr);
-    else
+    else {
         mGame_ptr->mPosMove = get_possible_move(mGame_ptr);
+        if (this->bot_think()) {
+            this->bot_move();
+        }
+    }
 }
 
 bool GameBot::bot_think() {
